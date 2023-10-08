c2173c2eb65babef4d9986103836e27f311dae08
wallacemod <wallacepatos@hotmail.com>
2023-10-07 21:23:37 -0300

    Project created

---
+++ src/test/java/com/wallacemod/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacemod.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
