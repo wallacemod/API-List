ccd2ab9f8591d94b6b119d92b25f62c92e96484d
wallacemod <wallacepatos@hotmail.com>
2023-10-09 00:33:26 -0300

    add POST move games on list

--- src/main/java/com/wallacemod/dslist/services/GameListService.java
+++ src/main/java/com/wallacemod/dslist/services/GameListService.java
@@ -4,6 +4,7 @@ import com.wallacemod.dslist.dto.GameListDTO;
 import com.wallacemod.dslist.dto.GameMinDTO;
 import com.wallacemod.dslist.entities.Game;
 import com.wallacemod.dslist.entities.GameList;
+import com.wallacemod.dslist.projections.GameMinProjection;
 import com.wallacemod.dslist.repositories.GameListRepository;
 import com.wallacemod.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex; //ternário
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for(int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
